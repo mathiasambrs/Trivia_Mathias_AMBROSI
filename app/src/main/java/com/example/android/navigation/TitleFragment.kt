0227e5b3dd7e524b8b88a9bc9a3581f8b49d012a
mathiasambrs <mathias.ambrs@eduge.ch>
2023-02-06 13:37:37 +0100

    Commit step 19

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -41,9 +41,10 @@ class TitleFragment : Fragment() {
         val  binding: FragmentTitleBinding =DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
         )
-        binding.playButton.setOnClickListener (
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-                )
+        binding.playButton.setOnClickListener { v: View ->
+            v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+            //(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+                }
         setHasOptionsMenu(true)
         //{ view: View ->
             //Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
